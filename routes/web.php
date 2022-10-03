a1a48d1f838a2eb169f1cbc5b50aa327798a1d7a
levYyyyy <levydevelope@gmail.com>
2022-10-03 20:58:25 +0300

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('api')->group(static function() {
+    Route::prefix('auth')->group(static function() {
+        Route::get('/login', [LoginController::class, 'show']);
+        Route::post('/login', [LoginController::class, 'login']);
+    
+        Route::get('/register', [RegisterController::class, 'show']);
+        Route::post('/register', [RegisterController::class, 'register']);
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::middleware('auth')->group(static function() {
+        Route::get('/me', [UserController::class, 'index']);
+        Route::get('/user/{id}', [UserController::class, 'show']);
+
+        Route::post('/video', [VideController::class, 'store']);
+        Route::get('/video/{id}', [VideController::class, 'show']);
+        
+        Route::post('/video{id}', [CommentController::class, 'store']);
+    });
 });
