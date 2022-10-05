edb6279f77d62495c275d3c4cb3e41cbd79a0d40
levYyyyy <levydevelope@gmail.com>
2022-10-05 18:57:20 +0300

    authorization system

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
+    public function show() {
+        dd('login page');
+    }
+    
+    public function login(Request $request) {
+        
+        $user = User::where('name', $request->input('name'))->first();
+        
+        if( Hash::check($request->input('password'), $user['password']) ) {
+            return "authorized";
+        }
+        else {
+            return "undefined credentials";
+        }
+    }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function show() {
+        dd('register page');
+    }
+    
+    public function register(Request $request) {
+        
+        // TODO: Do some validation
+        $user = [
+            'email' => $request->input('email'),
+            'name' => $request->input('name'),
+            'password' => Hash::make($request->input('password')),
+        ];
+
+        return User::create($user);
+    }
 }
